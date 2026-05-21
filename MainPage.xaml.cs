ec1f58492780e4fa557af933f40b446505bc9f0f
Dylan-Marinss <dylanmarins24@gmail.com>
2026-05-20 21:42:22 -0300

    Atividade

---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppRpgEtec
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        } 
+    }
+}
